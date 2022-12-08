add82882a40320f0769d7cceca25cbb99e362274
jsh-1235 <jsh-1235@hanmail.net>
2022-12-08 13:36:07 +0900

    initialize

---
+++ public/area.js
@@ -0,0 +1,4 @@
+"use strict";
+export default function area(arg1, arg2) {
+  return arg1 * arg2;
+}
---
+++ public/index.js
@@ -0,0 +1,11 @@
+// "use strict";
+
+import area from "./area.js";
+
+window.addEventListener("load", (event) => {
+  // console.table(event);
+
+  console.log(area(10, 50));
+
+  console.log("Window Loaded.");
+});
